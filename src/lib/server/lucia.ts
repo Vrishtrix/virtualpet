a8547bea2879351ba09050bd6647057b9830e6cb
Vrishin Patel <vrishin_patel@outlook.com>
2023-05-01 21:36:51 +0530

    build(deps): add github oauth using lucia

--- src/lib/server/lucia.ts
+++ src/lib/server/lucia.ts
@@ -1,15 +1,22 @@
 import lucia from 'lucia-auth';
+import { github } from '@lucia-auth/oauth/providers';
 import { sveltekit } from 'lucia-auth/middleware';
 import prisma from '@lucia-auth/adapter-prisma';
 
 import { PrismaClient } from '@prisma/client';
 
 import { dev } from '$app/environment';
+import { GITHUB_CLIENT_ID, GITHUB_CLIENT_SECRET } from '$env/static/private';
 
 export const auth = lucia({
 	adapter: prisma(new PrismaClient()),
 	env: dev ? 'DEV' : 'PROD',
 	middleware: sveltekit(),
+});
+
+export const githubAuth = github(auth, {
+	clientId: GITHUB_CLIENT_ID,
+	clientSecret: GITHUB_CLIENT_SECRET,
 });
 
 export type Auth = typeof auth;
